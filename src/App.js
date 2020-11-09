8c8eb2b320eb3d845f7eda2cd9246ff8fdcb8a35
shohanurr <sohanbaf@gmail.com>
2020-11-10 01:50:46 +0600

    Adding Siebar chat info component here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-      
-        
+        <Sidebar/>
+        {/*chats */}
       </div>  
     </div>
   );
---
+++ src/components/sidebar/SidebarChat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SidebaChat.css'
+
+function SidebarChat() {
+    return (
+        <div className="sidebarChat">
+           <h1>Hey</h1> 
+        </div>
+    )
+}
+export default SidebarChat
